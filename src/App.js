abd10f348ad2c9dd8bbdb65a02bd3f078a5fd8b9
Ines Serra <ines.isfserra@gmail.com>
2021-10-31 14:13:12 +0000

    add github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a
+          href="https://github.com/inesserra/improved-weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Ines Serra
+      </footer>
+    </div>
+  );
 }
