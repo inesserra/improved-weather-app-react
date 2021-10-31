aac70312cf1d55296f74b0443921ddd520f1c39b
Ines Serra <ines.isfserra@gmail.com>
2021-10-31 21:40:49 +0000

    add animated image npm

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,11 +1,14 @@
 import React from "react";
 import FormatDate from "./FormatDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
     <div className="row MainDisplay">
       <div className="col-4">
-        <img src={props.data.iconUrl} alt="weather icon" />
+        <span className="float-left">
+          <WeatherIcon code={props.data.icon} alt={props.data.description} />
+        </span>
         <span className="Temperature">{props.data.temperature}</span>
         <span className="Celsius">°C</span> |{" "}
         <span className="Farenheight">°F</span>
