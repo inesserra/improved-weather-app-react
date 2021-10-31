e490b63e40fa53c6cfef0cad5e8799657249f13a
Ines Serra <ines.isfserra@gmail.com>
2021-10-31 15:42:50 +0000

    added CSS properties

--- src/Weather.js
+++ src/Weather.js
@@ -11,31 +11,37 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />{" "}
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />{" "}
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100 Button"
+            />{" "}
           </div>
         </div>
       </form>
-      <div className="row">
-        <div className="col-3">
+      <div className="row MainDisplay">
+        <div className="col-4">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weather icon"
           />
-          <span>14</span>
-          <span>°C</span> | <span>°F</span>
+          <span className="Temperature">14</span>
+          <span className="Celsius">°C</span> |{" "}
+          <span className="Farenheight">°F</span>
         </div>
-        <div className="col-3">
-          <ul>
+        <div className="col-4">
+          <ul className="RainWind">
             <li>Precipitation: 15%</li>
             <li>Humidity: 90%</li>
             <li>Wind: 11mph</li>
           </ul>
         </div>
-        <div className="col-6">
-          <h1>New York</h1>
+        <div className="col-4 CurrentTime">
+          <h1>New York, NY</h1>
           <ul>
             <li>Saturday 14:33</li>
             <li>Cloudy</li>
